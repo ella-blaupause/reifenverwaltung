90a40d8dcfe00d964efc96cb5028a74e060f7cdc
Ella Kempel <ella.lernt@mail.de>
2023-10-01 17:57:46 +0200

    display product name

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,7 +1,12 @@
-import {products} from "../data/products.js"
+import { products } from "../data/products";
 
 export default function ProductList(){
     return(
-        <p>hallo</p>
-    )
+        <>
+            {products.map((product)=>(
+                <p key={product.id}>{product.Name}</p>
+            ))}
+        </>
+
+    );
 }
