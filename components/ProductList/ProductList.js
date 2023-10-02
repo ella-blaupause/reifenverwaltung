2329cb4a6a1e32a641e333e445beecae1e1110d5
Ella Kempel <ella.lernt@mail.de>
2023-10-02 09:12:00 +0200

    add picture

--- components/ProductList/ProductList.js
+++ components/ProductList/ProductList.js
@@ -1,16 +1,23 @@
-import { Fragment } from "react";
 import { products } from "../../data/products";
-import "./ProductList.css"
+import styles from "./ProductList.module.css"
+import Image from 'next/image'
+
 
 
 export default function ProductList(){
+
     return(
-        <ul className="ul-style">
+        <ul className={styles.list}>
             {products.map((product)=>(
-                <Fragment key={product.id}>
-                    <li >{product.Name}</li>
-                    <li >{product.Größe}</li>
-                </Fragment>
+                <div key={product.id} className={styles.productCard}>
+                    <Image src={product.Bild} 
+                        alt=""  
+                        width={100} 
+                        height={100}/>
+                    <li>{product.Name}</li>
+                    <li>{product.Größe}</li>
+                    <li>{product.Saison}</li>
+                </div>
             ))}
         </ul>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import ProductList from "../../components/ProductList/ProductList"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <main>
-      <h1>Reifenverwaltung</h1>
+      <h1 className={styles.heading}>Reifenverwaltung</h1>
       <ProductList />
     </main>
   )
