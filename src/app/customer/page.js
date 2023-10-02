4dec34cf732d4dd9d3505b8fd4851d2ac3725d04
Ella Kempel <ella.lernt@mail.de>
2023-10-02 12:50:11 +0200

    create navugation to customer page

--- src/app/customer/page.js
+++ src/app/customer/page.js
@@ -1,10 +1,11 @@
+import Heading from "../../../components/Heading/Heading"
 import ProductList from "../../../components/ProductList/ProductList" 
-import styles from "./customer.module.css"
+
 
 export default function Customor() {
   return (
     <main>
-      <h1 className={styles.heading}>Reifenverwaltung</h1>
+      <Heading />
       <ProductList />
     </main>
   )
