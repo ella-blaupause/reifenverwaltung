5585f8190b5bade0d0b85963515e3886e8316b6a
Ella Kempel <ella.lernt@mail.de>
2023-10-14 11:19:03 +0200

    connect editProduct page with session

--- src/app/editProduct/[id]/page.js
+++ src/app/editProduct/[id]/page.js
@@ -4,14 +4,34 @@ import { useEffect, useState } from "react";
 import EditProductForm from "../../../../components/EditProductForm/EditProductForm";
 import Heading from "../../../../components/Heading/Heading";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function EditProduct({ params }) {
   const { id } = params;
-
   const { data, isLoading } = useSWR(id ? `/api/products/${id}` : null, fetcher);
-  
+  const { data: session } = useSession(); 
+
+  if(!session) {
+      return(
+        <>
+          <Heading />
+          <Link href={"/login"}>
+              Bitte einlogen <span>Login</span>
+          </Link>
+        </>
+      )
+    } else if( session.user.role !== "admin"){
+      return (
+          <>
+            <Heading />
+            <h3>Du bist kein Admin!</h3>
+            <Link href={"/dashboard"}>Zurück zum Kundenbereich </Link>
+          </>
+      )
+  }
 
   if (!data) return;
 
@@ -25,7 +45,7 @@ export default function EditProduct({ params }) {
 
   return (
     <>
-        <Heading />
+      <Heading />
        <EditProductForm id={id} Name={Name} Bild={Bild} Größe={Größe} Saison={Saison}  />
     </>
   )
